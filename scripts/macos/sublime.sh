d6871e0d20fac53ec49e7f67801a67752307a320
Jake Stanley <mail@jakestanley.co.uk>
2017-04-05 13:20:38 +0100

    Improved sublime.sh macos script (set it up for future cross compatibility with Linux)

--- scripts/macos/sublime.sh
+++ scripts/macos/sublime.sh
@@ -1,2 +1,23 @@
-rm -r ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-ln -s $DOTFILES_REPO_DIR/config/sublime-text-3/Packages/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+#!/bin/sh
+
+# link or relink sublime text 3 config
+
+OS=`uname -s`
+echo $OS
+
+if [ "$OS" == 'Darwin' ]; then
+    SUBLIME_DIR="$HOME/Library/Application Support/Sublime Text 3"
+else
+    # SUBLIME_DIR="$HOME/.config/sublime-text-3"
+    echo "Not Darwin"
+    exit -1
+fi
+
+echo "sublime dir..."
+echo $SUBLIME_DIR
+echo $DOTFILES_REPO_DIR
+
+mkdir -p "$SUBLIME_DIR/Packages"
+rm -rf   "$SUBLIME_DIR/Packages/User"
+ln -s    "$DOTFILES_REPO_DIR/config/sublime-text-3/Packages/User" \
+         "$SUBLIME_DIR/Packages/User"
