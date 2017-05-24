c89fe389921fc3ff3254c45ddf10ee556bfb9c76
Jake Stanley <jacob.stanley@hermes-europe.co.uk>
2017-05-24 14:19:06 +0100

    added xinitrc conditional to launch "VBoxClient-all" if it's detected

--- xinitrc.sh
+++ xinitrc.sh
@@ -6,6 +6,10 @@ xrdb -merge ~/.Xresources &
 # set up monitors configuration
 ~/.xinitrc.d/monitors.sh &
 
+if which VBoxClient-all >/dev/null; then
+    VBoxClient-all &
+fi
+
 # compton (if it's installed and that's your thing)
 if which compton >/dev/null; then
     compton -b &
