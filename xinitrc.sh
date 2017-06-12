3d10a578be7b753e6f67b96659a1ba8348228f6c
Jake Stanley <mail@jakestanley.co.uk>
2017-06-12 11:13:56 +0100

    removed conditional dunst positioning centre stuff. too much ball ache

--- xinitrc.sh
+++ xinitrc.sh
@@ -33,21 +33,8 @@ if which compton >/dev/null; then
     # compton -b &
 fi
 
-# set screen width variable
-export X_SCREEN_WIDTH=$(xrandr -q | awk -F'current' -F',' 'NR==1 {gsub("( |current)","");print $2}' | grep "[0-9]*" -oh | head -n 1 | tail -n 1)
-
 # dunst (if it's installed and that's your thing)
 if which dunst >/dev/null; then
-
-    # dynamically calculate dunst dims and position
-    # dunst_width=350
-    # dunst_height=50
-
-    # dunst_x=$[$[$[$X_SCREEN_WIDTH] / 2] - $[$dunst_width / 2]]
-    # dunst_y=30
-
-    # start with geometry as dynamic stuff can't be done in config
-    # dunst -geometry ${dunst_width}x${dunst_height}+${dunst_x}+${dunst_y} &
     dunst
 fi
 
