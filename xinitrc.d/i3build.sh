4b9de8e9f7c8ac3ace84023d20d9fc35312f8c56
Jake Stanley <mail@jakestanley.co.uk>
2017-07-03 09:43:46 +0100

    i3 themes refactoring

--- xinitrc.d/i3build.sh
+++ xinitrc.d/i3build.sh
@@ -15,7 +15,7 @@ cat \
     ~/.config/i3/config.d/bindings-i3.cnf \
     ~/.config/i3/config.d/bindings-other.cnf \
     ~/.config/i3/config.d/bindings-music.cnf \
-    ~/.config/i3/config.d/${THEME}.cnf \
+    ~/.config/i3/themes.d/${THEME}.cnf \
     ~/.config/i3/config.d/colors.cnf \
     ~/.config/i3/config.d/keys.cnf \
     ~/.config/i3/config.d/mode-resize.cnf \
