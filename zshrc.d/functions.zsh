6b85c38e98fa76debeabf27cf9d51a594bcf47b0
Jake Stanley <mail@jakestanley.co.uk>
2017-10-19 10:06:50 +0100

    add docker_bash function. spins up a docker instance and bashes into it lol

--- zshrc.d/functions.zsh
+++ zshrc.d/functions.zsh
@@ -4,3 +4,8 @@
 function docker_ssh() {
     ssh root@$(docker inspect $1 | jq '.[0].NetworkSettings.Networks.bridge.IPAddress' --raw-output);
 }
+
+# bash from within a docker instance. just pass the docker image name. very basic
+function docker_bash() {
+    docker run -it --entrypoint /bin/bash $1
+}
