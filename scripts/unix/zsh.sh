512ef75098afa132d51564fc26aef01cac180b83
Jake Stanley <mail@jakestanley.co.uk>
2018-01-08 15:39:20 +0000

    added zprofile for zsh login shells and run docker in it if it exists

--- scripts/unix/zsh.sh
+++ scripts/unix/zsh.sh
@@ -6,9 +6,11 @@ if [[ "${DOTFILES_REPO_DIR}" == "" ]]; then
 fi
 
 rm -r ~/.aliases
+rm -r ~/.zprofile
 rm -r ~/.zshrc
 rm -r ~/.zshrc.d
 
 ln -s $DOTFILES_REPO_DIR/aliases.sh ~/.aliases
+ln -s $DOTFILES_REPO_DIR/zprofile.sh ~/.zprofile
 ln -s $DOTFILES_REPO_DIR/zshrc.sh ~/.zshrc
 ln -s $DOTFILES_REPO_DIR/zshrc.d ~/.zshrc.d
---
+++ zprofile.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+# run dropbox if it's installed                                                 
+if which dropbox >/dev/null; then                                               
+    dropbox &                                                                   
+fi                                                                              
+
