29b931d21d50d7c851ae0b7ca704fd4ff6eb78f4
Jake Stanley <mail@jakestanley.co.uk>
2018-01-08 15:52:37 +0000

    quietly remove files when running zsh.sh and print startup files link

--- scripts/unix/zsh.sh
+++ scripts/unix/zsh.sh
@@ -5,12 +5,16 @@ if [[ "${DOTFILES_REPO_DIR}" == "" ]]; then
     exit 1
 fi
 
-rm -r ~/.aliases
-rm -r ~/.zprofile
-rm -r ~/.zshrc
-rm -r ~/.zshrc.d
+rm -rf ~/.aliases
+rm -rf ~/.zprofile
+rm -rf ~/.zshrc
+rm -rf ~/.zshrc.d
 
 ln -s $DOTFILES_REPO_DIR/aliases.sh ~/.aliases
 ln -s $DOTFILES_REPO_DIR/zprofile.sh ~/.zprofile
 ln -s $DOTFILES_REPO_DIR/zshrc.sh ~/.zshrc
 ln -s $DOTFILES_REPO_DIR/zshrc.d ~/.zshrc.d
+
+# useful information
+_zsh_site="http://zsh.sourceforge.net/Intro/intro_3.html"
+echo "Read ${_zsh_site} for more information on Zsh startup files"
