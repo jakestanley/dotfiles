b2e9d2fbb30334aa26605ffa5d0c837f71253b04
Jake Stanley <mail@jakestanley.co.uk>
2018-01-24 11:09:42 +0000

    changed 'DOTFILES_REPO_DIR' alias to just 'DOTFILES' (for chromium.sh)

--- scripts/linux/chromium.sh
+++ scripts/linux/chromium.sh
@@ -3,11 +3,11 @@
 
 # link or relink git
 
-if [[ "${DOTFILES_REPO_DIR}" == "" ]]; then
-    echo "DOTFILES_REPO_DIR is not set. Exiting to stay safe"
+if [[ "${DOTFILES}" == "" ]]; then
+    echo "DOTFILES is not set. Exiting to stay safe"
     exit 1
 fi
 
 rm -rf ~/.config/chromium-flags.conf
 
-ln -s $DOTFILES_REPO_DIR/config/chromium-flags.conf ~/.config/chromium-flags.conf
+ln -s $DOTFILES/config/chromium-flags.conf ~/.config/chromium-flags.conf
