d39b03f78053ebe7be0a31afcff1d94129041cfd
Jake Stanley <mail@jakestanley.co.uk>
2018-03-19 09:19:19 +0000

    stop starting dropbox on zprofile as it spams shells

--- zprofile.sh
+++ zprofile.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env zsh
 
-# run dropbox if it's installed                                                 
-if which dropbox >/dev/null; then                                               
-    dropbox &                                                                   
-fi                                                                              
-
