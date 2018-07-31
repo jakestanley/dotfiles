8abacd42b10cb6e3073a4c406d232da9d7628f5c
Jake Stanley <mail@jakestanley.co.uk>
2018-07-31 13:16:16 +0100

    move some environment variables into zprofile and clean up zshrc

--- zshrc.sh
+++ zshrc.sh
@@ -3,11 +3,6 @@
 # Path to your oh-my-zsh installation.
 export ZSH=$HOME/.oh-my-zsh
 
-# export DOTFILES_DIR
-if [[ "$DOTFILES" == "" ]]; then
-    export DOTFILES=$HOME/dotfiles
-fi
-
 # export user theme
 if [[ "$THEME" == "" ]]; then
     export THEME="base16-solarized-dark"
@@ -120,25 +115,10 @@ echo ""
 
 # User configuration
 export CM_LAUNCHER=rofi
-export VISUAL="vim"
-export SVN_EDITOR="vim"
-export PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/go/bin:\
-$HOME/opt/jdk/jre/bin:/usr/bin/core_perl:\
-$HOME/.pilau/lemonbar:$HOME/opt/sqldeveloper:\
-$HOME/git/hermes/tools/bin:\
-$HOME/bin:\
-$HOME/.local/bin:\
-$HOME/.npm/.bin"
-
-export DOTFILES="${HOME}/dotfiles"
-export GOPATH="${HOME}/go"
 
 if [ "$OS" = 'Darwin' ]; then
     export PATH="$HOME/Library/Android/sdk/platform-tools:$PATH"
     export PATH="$HOME/Library/Python/3.6/bin:$PATH"
-else
-    export JAVA_HOME="/usr/lib/jvm/java-8-openjdk"
-    # insert linux specific PATH stuff here
 fi
 export MANPATH="/usr/local/man:$MANPATH"
 
@@ -151,16 +131,6 @@ fi
 bindkey '[C' forward-word
 bindkey '[D' backward-word
 
-# You may need to manually set your language environment
-export LANG=en_GB.UTF-8
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
-
 # Compilation flags
 # export ARCHFLAGS="-arch x86_64"
 
@@ -170,28 +140,20 @@ export SSH_KEY_PATH="~/.ssh/id_rsa"
 # shellcheck
 export SHELLCHECK_OPTS="-e SC2086"
 
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
-source ~/.aliases
-source ~/.zshrc.d/functions.zsh
-
 # create work aliases/functions file if it doesn't exist as the file should
 # not be persisted in a public repository for security reasons
 if [ ! -f ~/.zshrc.d/work.zsh ]; then
     echo "#!/usr/bin/env zsh" >> ~/.zshrc.d/work.zsh
     chmod u+x ~/.zshrc.d/work.zsh
 fi
+
+# aliases and functions
+source ~/.aliases
+source ~/.zshrc.d/functions.zsh
 source ~/.zshrc.d/work.zsh
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 setopt hist_ignorealldups
 
-export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
