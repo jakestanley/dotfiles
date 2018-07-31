833193c20f7e83bf1ccecb8a1798ae7238869a8f
Jake Stanley <mail@jakestanley.co.uk>
2018-07-31 13:21:04 +0100

    set lemonbar offset dimensions defaults

--- pilau/lemonbar/lemonbar_dimensions.sh
+++ pilau/lemonbar/lemonbar_dimensions.sh
@@ -5,8 +5,14 @@
 SCREEN_WIDTH=$(xrandr -q | grep -o "current\s[0-9]\{3,4\}" | grep -o "[0-9]\{3,4\}")
 
 # set bar dimensions
-BAR_OFFSET_X="0"
-BAR_OFFSET_Y="0"
+if [[ "${BAR_OFFSET_X}" == "" ]]; then
+    BAR_OFFSET_X="440"
+fi
+
+if [[ "${BAR_OFFSET_Y}" == "" ]]; then
+    BAR_OFFSET_Y="8"
+fi
+
 BAR_HEIGHT="24"
 BAR_WIDTH=$(($SCREEN_WIDTH-$((BAR_OFFSET_X*2))))
 
