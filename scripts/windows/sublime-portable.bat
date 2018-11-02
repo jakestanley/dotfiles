774261d3a297bd955e2065372595f46adf82bbd6
Jake Stanley <mail@jakestanley.co.uk>
2018-11-02 11:24:43 +0000

    add linker script for portable sublime text (assumes things)

---
+++ scripts/windows/sublime-portable.bat
@@ -0,0 +1,2 @@
+rmdir /s /q "%UserProfile%\opt\sublime-text\Data\Packages\User"
+mklink /J "%UserProfile%\opt\sublime-text\Data\Packages\User" "%UserProfile%\git\github.com\madstanners\dotfiles\config\sublime-text-3\Packages\User"
