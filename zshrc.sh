5a07701582bd42e060cfffe1e3f1280d67308e7b
Jake Stanley <mail@jakestanley.co.uk>
2018-11-09 16:02:20 +0000

    trying agnoster theme with powerline

--- zshrc.sh
+++ zshrc.sh
@@ -17,7 +17,8 @@ fi
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-ZSH_THEME="lukerandall"
+ZSH_THEME="agnoster"
+#ZSH_THEME="lukerandall"
 
 # Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
