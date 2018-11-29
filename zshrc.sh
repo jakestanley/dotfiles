4e5070a20697b582fa843938072e0b3f409ee3f2
Jake Stanley <mail@jakestanley.co.uk>
2018-11-29 11:40:25 +0000

    add sdkman to zshrc.sh... there's probably somewhere else this should go but hey ho

--- zshrc.sh
+++ zshrc.sh
@@ -158,3 +158,7 @@ setopt hist_ignorealldups
 
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="/home/vagrant/.sdkman"
+[[ -s "/home/vagrant/.sdkman/bin/sdkman-init.sh" ]] && source "/home/vagrant/.sdkman/bin/sdkman-init.sh"
