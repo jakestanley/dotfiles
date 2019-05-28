86cae87699e026cd4616424cf1d8ddc42e514037
Jake Stanley <mail@jakestanley.co.uk>
2019-05-28 09:25:24 +0100

    merged origin/master with work config

--- zshrc.sh
+++ zshrc.sh
@@ -12,7 +12,7 @@ fi
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-#ZSH_THEME="agnoster"
+
 ZSH_THEME="lukerandall"
 
 # Uncomment the following line to use case-sensitive completion.
